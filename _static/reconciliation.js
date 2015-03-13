72389fd705904e86eb1d04c999dad59077c76cc6
Xavier Morel <xmo@odoo.com>
2015-03-13 12:26:23 +0100

    [IMP] reconciliation demo
    * multi-step process
    * unreconcile button/step
    * stepped animation

--- _static/reconciliation.js
+++ _static/reconciliation.js
@@ -1,9 +1,27 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
+        var state = 0;
+        var operations = [
+            function reconcile2() {
+                $rec.addClass('reconcile2');
+                return 1;
+            },
+            function reconcile1() {
+                $rec.addClass('reconcile1');
+                return 2;
+            },
+            function unreconcile() {
+                $rec.removeClass('reconcile1 reconcile2');
+                $1.add($2).removeClass('reconciled');
+                setTimeout(update_btn, 0);
+                return 0;
+            }
+        ];
         var $rec = $('#reconciliation #example');
-        var $btn = $('<button class="btn btn-success" type="button">')
-            .text("Reconcile")
-            .appendTo($rec);
+        var $buttons = $('<div class="buttons">').on('click', 'button', function () {
+            this.disabled = true;
+            state = operations[state]();
+        }).appendTo($rec);
 
         var $1 = $rec.find('td:contains("Invoice 1"), td:contains("Payment 1")')
             .parent()
@@ -12,23 +30,35 @@
             .parent()
             .addClass('invoice2');
 
-        // will be called multiple times (each tr + each td), doesn't matter
-        // since the behavior is constant
+        // will be called multiple times (each tr + each td), only take trs in
+        // account
         $rec.on('animationend webkitAnimationEnd MSAnimationEnd', function (e) {
-            switch (e.originalEvent.animationName) {
-            case 'invoice1':
-                $1.hide();
+            switch (e.originalEvent.target) {
+            case $1[0]:
+                $1.addClass('reconciled');
                 break;
-            case 'invoice2':
-                $2.hide();
+            case $2[0]:
+                $2.addClass('reconciled');
                 break;
             }
+            update_btn();
         });
+        update_btn();
 
-        $btn.click(function () {
-            $rec.addClass('reconcile-accounts');
-            $btn.prop('disabled', true);
-        });
+        function update_btn() {
+            var $reconcile = $('<button class="btn btn-success" type="button">')
+                .text("Reconcile")
+                .appendTo($buttons.empty())
+            switch (state) {
+            case 0: case 1: break;
+            case 2:
+                $reconcile.prop('disabled', true);
+                $('<button class="btn btn-primary" type="button">')
+                    .text("Unreconcile")
+                    .appendTo($buttons);
+                break;
+            }
+        }
     });
 })();
 
