4030182881afe319692184d59a9e79b6513620a3
Xavier Morel <xmo@odoo.com>
2015-04-15 14:32:24 +0200

    [FIX] warning when compiling doc

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ html_theme_path = ['_themes']
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_style = 'odoodoc.css'
-html_add_permalinks = False
+html_add_permalinks = ''
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
