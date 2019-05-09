e806e442edb935d505a57b4dae121a862733fa31
Basit Mustafa <basit.mustafa@gmail.com>
2019-05-08 18:28:01 -0700

    tzdata non interactive

--- devops/build.sh
+++ devops/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 apt-get update
+ln -fs /usr/share/zoneinfo/Europe/Dublin /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
 apt-get install python3-sphinx build-essential -y
 make html
 ls _build/html/
