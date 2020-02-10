9323cba5ceae2d57e61208bd049b6f492ab2c414
Antoine Vandevenne (anv) <anv@odoo.com>
2020-02-10 16:56:13 +0100

    [FIX] config: fix path of odoo logo

--- conf.py
+++ conf.py
@@ -241,7 +241,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_static/banners/odoo_logo.png'
+latex_logo = '_static/icons/odoo_logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
