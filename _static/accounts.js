cd6b89ef9fae4d3bfc4644484ebcce44ebf155af
Mohammed Shekha <msh@openerp.com>
2020-09-28 17:47:49 +0530

    [IMP] Improvement

--- _static/accounts.js
+++ _static/accounts.js
@@ -4,45 +4,45 @@
     const {Component, useState} = owl;
     const {xml} = owl.tags;
 
-    class Account extends Component {
+    class AccountsTable extends Component {
 
         constructor() {
             super(...arguments);
-            this.state = useState({section: ''});
+            this.state = useState({current: ''});
         }
 
         mounted() {
-            var list = document.querySelectorAll('.intro-list p');
+            const list = document.querySelectorAll('.intro-list p');
             Array.prototype.forEach.call(list, (node) => {
-                node.addEventListener('mouseover', (e) => {
-                    if (!e.currentTarget.contains(e.target)) {
+                node.addEventListener('mouseover', (ev) => {
+                    if (!ev.currentTarget.contains(ev.target)) {
                         return;
                     }
-                    e.currentTarget.classList.add('secondary');
-                    var target = e.currentTarget.className.split(/\s+/).reduce((acc, cls) => {
+                    ev.currentTarget.classList.add('secondary');
+                    const target = ev.currentTarget.className.split(/\s+/).reduce((acc, cls) => {
                         if (acc) {
                             return acc;
                         }
                         var m = /^intro-(.*)$/.exec(cls);
                         return m && m[1];
                     }, null);
-                    this.state.section = target;
+                    this.state.current = target;
                 });
-                node.addEventListener('mouseout', (e) => {
-                    e.currentTarget.classList.remove('secondary');
-                    this.state.section = "";
+                node.addEventListener('mouseout', (ev) => {
+                    ev.currentTarget.classList.remove('secondary');
+                    this.state.current = "";
                 });
             });
         }
 
         static template = xml`
             <div style="margin-top:1em;">
-                <div t-att-class="state.section === 'p-l' ? 'related' : ''">
+                <div t-att-class="state.current === 'p-l' ? 'related' : ''">
                     <h4>Profit &amp; Loss</h4>
                     <div>
-                        <h5 t-att-class="state.section === 'retained' ? 'secondary' : ''">Net Profit</h5>
-                        <div t-att-class="state.section === 'gross-profit' ? 'secondary' : ''">
-                            <h5 t-att-class="state.section === 'gross-profit' ? 'related' : ''">Gross Profit</h5>
+                        <h5 t-att-class="state.current === 'retained' ? 'secondary' : ''">Net Profit</h5>
+                        <div t-att-class="state.current === 'gross-profit' ? 'secondary' : ''">
+                            <h5 t-att-class="state.current === 'gross-profit' ? 'related' : ''">Gross Profit</h5>
                             <dl>
                                 <dt>Revenue</dt>
                                 <dd>Revenue</dd>
@@ -50,7 +50,7 @@
                                 <dd>Cost of Goods Sold</dd>
                             </dl>
                         </div>
-                        <div t-att-class="state.section === 'opex' ? 'related' : ''">
+                        <div t-att-class="state.current === 'opex' ? 'related' : ''">
                             <h5>Operating Income or Loss</h5>
                             <dl>
                                 <dt><span>Less </span><span>Operating Expenses</span></dt>
@@ -65,32 +65,32 @@
                         </dl>
                     </div>
                 </div>
-                <div t-att-class="state.section === 'balance' ? 'related' : ''">
+                <div t-att-class="state.current === 'balance' ? 'related' : ''">
                     <h4>Balance Sheet</h4>
                     <div>
                         <h5>Net Assets</h5>
                         <div>
-                            <h5 t-att-class="state.section === 'assets' ? 'related' : ''">Total Assets</h5>
-                            <dl t-att-class="state.section === 'assets' ? 'secondary' : ''">
+                            <h5 t-att-class="state.current === 'assets' ? 'related' : ''">Total Assets</h5>
+                            <dl t-att-class="state.current === 'assets' ? 'secondary' : ''">
                                 <dt>Current Assets</dt>
                                 <dd><span>Cash &amp; Bank Accounts</span><br/><span>Accounts Receivable</span><br/><span>Deferred Tax Assets</span></dd>
                                 <dt><span>Plus </span><span>Non-current Assets</span></dt>
                                 <dd><span>Land &amp; buildings</span><br/><span>Intangible Assets</span></dd>
                             </dl>
                         </div>
-                        <dl t-att-class="state.section === 'liabilities' ? 'related' : ''">
+                        <dl t-att-class="state.current === 'liabilities' ? 'related' : ''">
                             <dt><span>Less </span><span>Current Liabilities</span></dt>
                             <dd><span>Accounts Payable</span><br/><span>Deferred Revenue</span><br/><span>Deferred Tax Liabilities</span></dd>
                             <dt><span>Less </span><span>Non-current liabilities</span></dt>
                             <dd>Long-term loans</dd>
                         </dl>
                     </div>
-                    <div t-att-class="state.section === 'equity' ? 'secondary' : ''">
-                        <h5 t-att-class="state.section === 'equity' ? 'related' : ''">Total Equity</h5>
+                    <div t-att-class="state.current === 'equity' ? 'secondary' : ''">
+                        <h5 t-att-class="state.current === 'equity' ? 'related' : ''">Total Equity</h5>
                         <dl>
                             <dt>Equity</dt>
                             <dd>Common Stock</dd>
-                            <dt t-att-class="state.section === 'retained' ? 'related' : ''"><span>Plus </span><span>Retained Earnings</span></dt>
+                            <dt t-att-class="state.current === 'retained' ? 'related' : ''"><span>Plus </span><span>Retained Earnings</span></dt>
                         </dl>
                     </div>
                 </div>
@@ -98,9 +98,9 @@
     }
 
     document.addEventListener('DOMContentLoaded', function () {
-        var target = document.querySelector('.accounts-table');
+        const target = document.querySelector('.accounts-table');
         if (!target) { return; }
-        const account = new Account();
-        account.mount(target);
+        const accountsTable = new AccountsTable();
+        accountsTable.mount(target);
     });
 }());
