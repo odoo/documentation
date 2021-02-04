12e78e95ec731b45e3929114cdbe571d907ad32c
Victor Feyens <vfe@odoo.com>
2021-02-04 17:46:47 +0100

    bring back analytics logic in layout template

--- extensions/odoo_theme/static/js/google_analytics.js
+++
@@ -1,9 +0,0 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// VFE FIXME, the {{}} is jinja code, it cannot be moved in an independent js code :P
-ga('create', '{{ google_analytics_key }}', 'auto');
-ga('set', 'anonymizeIp', true);
-ga('send','pageview');
