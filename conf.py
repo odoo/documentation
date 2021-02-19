d0dac6667d6b8567e11cff69cdd0b652c01d4206
Victor Feyens <vfe@odoo.com>
2021-02-19 11:24:35 +0100

    [FIX] favicon

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -166,7 +167,7 @@ html_theme_path = ['extensions']
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None  TODO ANVFE remove?
+html_favicon = os.path.join(html_theme_path[0], html_theme, 'static', 'img', 'favicon.ico')
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this
 # directory. They are copied after the builtin static files, so a file named "default.css" will
