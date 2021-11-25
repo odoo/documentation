f644d0bd5ecc3a448293970e6273ed9158e0a3eb
wan <wan@odoo.com>
2021-11-25 11:10:42 +0000

    [ADD] extension: allow to build graphs using graphviz
    X-original-commit: e00b5e35c76e824876bc965f6c414159b88feb86

---
+++ extensions/graphviz_placeholder/__init__.py
@@ -0,0 +1,25 @@
+from docutils.parsers.rst import Directive, directives
+from docutils import nodes
+
+
+class PlaceHolder(Directive):
+    """ Placeholder class for directives that must be skipped. """
+
+    has_content = True
+
+    def run(self):
+        node = nodes.literal_block('graphviz', '')
+        node += nodes.Text(
+            f'{self.content[0]}\n'
+            '> Graph not rendered because `dot` is not installed'
+        )
+        return [node]
+
+
+def setup(app):
+    directives.register_directive('graphviz', PlaceHolder)
+
+    return {
+        'parallel_read_safe': True,
+        'parallel_write_safe': True
+    }
