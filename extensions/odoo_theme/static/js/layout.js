ef83ef2802e161e5fa8105d2ec97bb4b3815cf35
Antoine Vandevenne (anv) <anv@odoo.com>
2022-03-10 18:00:49 +0100

    [IMP] odoo_theme: open images in a modal
    task-2358082
    
    X-original-commit: 2a867b4f600265d46085a9f6d77dfdc7ca331257
    Co-authored-by: Elisabeth Dickinson (edi) <edi@odoo.com>

--- extensions/odoo_theme/static/js/layout.js
+++ extensions/odoo_theme/static/js/layout.js
@@ -3,9 +3,29 @@
     document.addEventListener('DOMContentLoaded', function () {
         const content = document.getElementById('o_content');
 
-        // Enforce the presence of the `img-fluid` class on all images in the body.
+        let imageModalId = 0;
         content.querySelectorAll('img').forEach(image => {
+            // Enforce the presence of the `img-fluid` class on all images.
             image.classList.add('img-fluid');
+
+            // Add a modal to each image.
+            const modalContainer = document.createElement('div');
+            modalContainer.classList.add('o_image_modal');
+            modalContainer.innerHTML = `<div class="modal fade" id="modal-${imageModalId}">
+                   <div class="modal-dialog modal-dialog-centered">
+                     <div class="modal-content">
+                       <div class="modal-body">
+                         <img src="${image.src}" alt="${image.alt}"/>
+                         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+            `;
+            image.parentNode.append(modalContainer);
+            image.setAttribute('data-bs-toggle', 'modal');
+            image.setAttribute('data-bs-target', `#modal-${imageModalId}`);
+            imageModalId++;
         });
 
         // Make all external links open in a new tab by default.
