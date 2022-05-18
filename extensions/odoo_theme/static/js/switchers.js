a1b70a1d6e817550716cd447c62512314519c9f0
Antoine Vandevenne (anv) <anv@odoo.com>
2022-05-18 15:23:27 +0200

    [REF] odoo_theme: simplify and clarify the switchers' fallback mechanism

---
+++ extensions/odoo_theme/static/js/switchers.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Enable fallback URLs for broken redirects from the version or language switchers.
+        _prepareSwitchersFallbacks();
+    });
+
+    /**
+     * Add event listeners on links in the version and language switchers.
+     *
+     * If a link is clicked, the user is redirected to the closest fallback URL (including the
+     * original target URL) that is available.
+     */
+    const _prepareSwitchersFallbacks = () => {
+        document.querySelectorAll('a[class="dropdown-item"]').forEach(element => {
+            element.addEventListener('click', event => {
+                if (element.hasAttribute('href')) {
+                    event.preventDefault();
+                    const fallbackUrls = _generateFallbackUrls(element.getAttribute('href'));
+                    const fallbackUrl = _getFirstValidUrl(fallbackUrls);
+                    window.location.href = fallbackUrl;
+                }
+            });
+        });
+    };
+
+})();
--- extensions/odoo_theme/static/js/utils.js
+++ extensions/odoo_theme/static/js/utils.js
@@ -63,3 +63,99 @@ const _prepareAccordion = (tocElement) => {
         tocEntryList.parentNode.insertBefore(tocEntryWrapper, tocEntryList);
     });
 };
+
+/**
+ * Generate a list of fallback URLs from the closest to the furthest of the target URL and
+ * return the first one that points to an existing resource.
+ *
+ * The generation consists of starting with the target URL and walking back toward the root of
+ * the documentation while alternating between including the original language or not, if it was
+ * included in the original URL. The last fallback URL is the root of the documentation with the
+ * version stripped off to redirect the user to the index of the default version.
+ *
+ * Example:
+ * 1. .../documentation/13.0/contributing/documentation.html
+ * 2. .../documentation/13.0/contributing.html
+ * 3. .../documentation/13.0
+ * 4. .../documentation/
+ *
+ * Example:
+ * 1. .../documentation/15.0/fr/administration/install.html
+ * 2. .../documentation/15.0/administration/install.html
+ * 3. .../documentation/15.0/fr/administration.html
+ * 4. .../documentation/15.0/administration.html
+ * 5. .../documentation/15.0/fr/
+ * 6. .../documentation/15.0/
+ * 7. .../documentation/
+ */
+const _generateFallbackUrls = (targetUrl) => {
+
+    const _deconstructUrl = (url) => {  // TODO test when no version
+        let version = '';
+        let language = '';
+        const originalPathParts = [];
+        for (let fragment of url.split('/').reverse()) {
+            if (fragment.match(/^(?:saas-)?\d{2}.\d|master$/)) {
+                version = fragment;
+                break; // The remaining fragments are not part of the original path.
+            } else if (fragment.match(/^[a-z]{2}(?:_[A-Z]{2})?$/)) {
+                language = fragment;
+            } else {
+                originalPathParts.unshift(fragment);
+            }
+        }
+        return [version, language, originalPathParts];
+    };
+
+    const [version, language, originalPathParts] = _deconstructUrl(targetUrl);
+    const urlParts = targetUrl.split('/');
+    const urlBaseParts = urlParts.slice(
+        0, urlParts.length - originalPathParts.length - (version ? 1 : 0) - (language ? 1 : 0)
+    );
+    const urlBase = urlBaseParts.join('/');
+
+    // Generate the fallback URLs.
+    const fallbackUrls = [];
+    for (let i = originalPathParts.length; i >= 0; i--) {
+        const fallbackPathParts = originalPathParts.slice(0, i);
+
+        // Append '.html' to the last path part if it is missing and the part is not the root.
+        if (
+            fallbackPathParts.length > 0
+            && !fallbackPathParts[fallbackPathParts.length - 1].endsWith('.html')
+        ) {
+            fallbackPathParts[fallbackPathParts.length - 1] += '.html';
+        }
+
+        // Build the fallback URL from the version, language and path parts, if any.
+        if (version && language)
+            fallbackUrls.push(
+                `${urlBase}/${version}/${language}/${fallbackPathParts.join('/')}`,
+                `${urlBase}/${version}/${fallbackPathParts.join('/')}`,
+            );
+        else if (version && !language)
+            fallbackUrls.push(`${urlBase}/${version}/${fallbackPathParts.join('/')}`);
+        else if (!version && !language)
+            fallbackUrls.push(`${urlBase}/${fallbackPathParts.join('/')}`);
+    }
+    fallbackUrls.push(`${urlBase}/`);
+    return fallbackUrls;
+};
+
+/**
+ * Iterate over the provided URLs and return the first one that points to a valid resource.
+ */
+const _getFirstValidUrl = (urls) => {
+    for (let url of urls) {
+        if (url.startsWith('file:///')) { // TODO MAT when does it starts with file:/// ?
+            return url;
+        } else {
+            fetch(url).then(response => {
+                if (response.ok) {
+                    return url;
+                }
+            });
+        }
+    }
+    return urls[0]; // No valid url found, return the first one.
+};
