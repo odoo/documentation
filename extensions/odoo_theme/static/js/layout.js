73e74c7eef4e3b87cdf59dbd884dc2f6390dc791
Elisabeth Dickinson <edi@odoo.com>
2022-06-08 10:09:10 +0000

    [IMP] design: add an icon behind external links
    The external links' target attribute is now also updated to open the
    link in a new tab. In addition, the "[source]" link of autodoc's models
    is moved right next to the name of the model to correctly position the
    icon.
    
    task-2790244
    
    closes odoo/documentation#2209
    
    X-original-commit: 343decf1923b0f6c63dcd524401431e8b7e7ea3b
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Co-authored-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- extensions/odoo_theme/static/js/layout.js
+++ extensions/odoo_theme/static/js/layout.js
@@ -3,10 +3,15 @@
     document.addEventListener('DOMContentLoaded', function () {
         const content = document.getElementById('o_content');
 
-        // Enforce the presence of the `img-fluid` class on all images in the body
+        // Enforce the presence of the `img-fluid` class on all images in the body.
         content.querySelectorAll('img').forEach(image => {
             image.classList.add('img-fluid');
         });
+
+        // Make all external links open in a new tab by default.
+        content.querySelectorAll('a.external').forEach(externalLink => {
+            externalLink.setAttribute('target', '_blank');
+        })
     });
 
 })();
