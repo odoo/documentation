7b266b99a6f1a58a8dfb094b1d0f69fae4cc5c88
Antoine Vandevenne (anv) <anv@odoo.com>
2023-03-27 09:37:57 +0000

    [IMP] supported_version: release saas-16.2
    closes odoo/documentation#3891
    
    X-original-commit: 697cdad097617f20dfa0a3131b0679de7ae5e31e
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- conf.py
+++ conf.py
@@ -211,6 +211,8 @@ sphinx.transforms.i18n.docname_to_domain = (
 # is populated. If a version is passed to `versions` but is not listed here, it will not be shown.
 versions_names = {
     'master': "Master",
+    'saas-16.2': "Odoo Online",
+    'saas-16.1': "Odoo Online",
     '16.0': "Odoo 16",
     'saas-15.2': "Odoo Online",
     '15.0': "Odoo 15",
