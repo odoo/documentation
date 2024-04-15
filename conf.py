79286e3c9ee29146d43d8f5c9e920890e7c26494
Alvaro Fuentes <afu@odoo.com>
2024-04-15 11:30:21 +0200

    [IMP] upgrade-util: link code documentation
    Add direct code documentation from upgrade-util repo.
    Adapt `extensions/github_link`.
    
    Original commit: 3352d339973338824b62a2f7f196ebd0aaf2bf81
    
    X-original-commit: 26fd5f319271c7cc3b96b501a1e77620286fcd46
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- conf.py
+++ conf.py
@@ -133,6 +133,24 @@ else:
         )
         odoo_dir_in_path = True
 
+if odoo_dir_in_path:
+    upgrade_util_dir = next(filter(Path.exists, [Path('upgrade-util'), Path('../upgrade-util')]), None)
+    if not upgrade_util_dir:
+        _logger.warning(
+            "Could not find Upgrade Utils sources directory in `upgrade_util`.\n"
+            "The developer documentation will be built but autodoc directives will be skipped.\n"
+            "In order to fully build the 'Developer' documentation, clone the repository with "
+            "`git clone https://github.com/odoo/upgrade-util` or create a symbolic link."
+        )
+        odoo_dir_in_path = False
+    else:
+        _logger.info(
+            "Found Upgrade Util sources in %(directory)s",
+            {'directory': upgrade_util_dir.resolve()},
+        )
+        from odoo import upgrade
+        upgrade.__path__.append(str((upgrade_util_dir / 'src').resolve()))
+
 # Mapping between odoo models related to master data and the declaration of the
 # data. This is used to point users to available xml_ids when giving values for
 # a field with the autodoc_field extension.
@@ -249,6 +267,9 @@ redirects_dir = 'redirects/'
 
 sphinx_tabs_disable_tab_closing = True
 sphinx_tabs_disable_css_loading = True
+
+# Autodoc ordering
+autodoc_member_order = 'bysource'
 
 #=== Options for HTML output ===#
 
